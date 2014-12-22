a6c30e55544496fd36d296e9ed9dc01872858313
Matyas Matyas <duplamatyi@gmail.com>
2014-12-22 23:56:23 +0100

    Changed the send controller output encoding to json. Cleared up the for_rest bundle config.

--- src/ChatBundle/Controller/MessageController.php
+++ src/ChatBundle/Controller/MessageController.php
@@ -9,6 +9,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\Util\Codes;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
@@ -49,10 +50,10 @@ class MessageController extends FOSRestController
             $em->persist($message);
             $em->flush();
 
-            return new Response('The message was successfully created.', Codes::HTTP_CREATED);
+            return new JsonResponse('The message was successfully created.', Codes::HTTP_CREATED);
         }
 
-        return new Response('The request could not be understood by the server.', Codes::HTTP_BAD_REQUEST);
+        return new JsonResponse('The request could not be understood by the server.', Codes::HTTP_BAD_REQUEST);
     }
 
     /**
